30f9af3e98e850d09c50de52f883cee7d48e0ff4
arumm25 <nadyaarum02@gmail.com>
2025-03-11 22:06:23 +0700

    TUGAS 1 (latihan Biodata)

---
+++ Biodata.java
@@ -0,0 +1,14 @@
+public class Biodata {
+
+    public static void main(String[] args) {
+        System.out.println("NIM          : A11.2023.15469");
+        System.out.println("NAMA         : NADYA ARUM PRAMESTI");
+        System.out.println("ALAMAT       : SEKAYU");
+        System.out.println("KOTA         : Semarang");
+        System.out.println("Kode Pos     : 545675");
+        System.out.println("Telp         : 0895606812020");
+        System.out.println("Email        : nadyaarum02@gmail.com");
+        System.out.println("IPK          : 3.8");
+        System.out.println("Predikat     : Cumlaude");
+    }
+}
