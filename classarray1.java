6bda64c4cf756a031bcb43a4714b823376c65ea6
arumm25 <nadyaarum02@gmail.com>
2025-03-31 23:03:56 +0700

    TUGAS PRATIKUM 5 (3)

---
+++ classarray1.java
@@ -0,0 +1,16 @@
+public class classarray1 {
+    public static void main(String[] args) {
+        int angka[] ={68,7,28,90,32};
+        System.err.println("Jumlah data = 5");
+        System.err.println("Data ke - 1 = " + angka[0]);
+        System.err.println("Data ke - 2 = " + angka[1]);
+        System.err.println("Data ke - 3 = " + angka[2]);
+        System.err.println("Data ke - 4 = " + angka[3]);
+        System.err.println("Data ke - 5 = " + angka[4]);
+        System.err.println("Hasil nilai [0] = " + angka[0]);
+        System.err.println("Hasil nilai [1] = " + angka[1]);
+        System.err.println("Hasil nilai [2] = " + angka[2]);
+        System.err.println("Hasil nilai [3] = " + angka[3]);
+        System.err.println("Hasil nilai [4] = " + angka[4]);
+    }
+}
